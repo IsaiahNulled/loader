eb8be47b0d7788361f90a7536db8d01ce62ea126
Isaiah <144067817+IsaiahNulled@users.noreply.github.com>
2026-03-01 02:13:02 -0500

    Delete full directory

--- full/User/images.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <d3d11.h>
-
-// Load a single item icon from file path
-ID3D11ShaderResourceView* LoadItemIcon(const std::string& path);
--- full/User/stb_image_impl.cpp
+++
@@ -1,2 +0,0 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include "thirdparty/stb/stb_image.h"
